4cb105b3aa4f6e2ba8c3eb0dab94f260a0893238
Danny Roubin <dannyroubin1@gmail.com>
2021-04-21 13:13:40 -0700

    quiz partially works, loss mechanic works and question progression works, parts not working are checking if question selected was correct, and win condition

---
+++ assets/js/questions.js
@@ -0,0 +1,44 @@
+
+var questions = [
+    {
+      title: "Question 1 here",
+      choices: [
+        "answer154353", "answer2", "answer3","answer4"
+      ],
+      answer: "answer154353"
+  
+    },
+    {
+        title: "Question 2 here",
+        choices: [
+          "dog", "answer2", "answer3", "answer4"
+        ],
+        answer: "dog"
+    
+      },
+    {
+        title: "Question 3 here",
+        choices: [
+          "cat", "answer2", "answer3", "answer4"
+        ],
+        answer: "cat"
+    
+      },
+    {
+        title: "Question 4 here",
+        choices: [
+          "cat dog", "answer2", "answer3", "answer4"
+        ],
+        answer: "answer2"
+    
+      },
+    {
+        title: "Question 5 here",
+        choices: [
+          "dogcat", "answer2", "answer3", "answer4"
+        ],
+        answer: "dogcat"
+    
+      }
+  ]
+  
--- assets/js/script.js
+++ assets/js/script.js
@@ -11,11 +11,13 @@ var timerElement = document.querySelector("#time");
 var correctCheck = document.querySelector("#correctCheck");
 var option = document.querySelector("#optionButtons")
 
-let randomQuestion;
-let currentQuestion;
+var currentQuestion;
+var currentQuestionIndex = 0;
 
 var questionCount = 0;
 var timerCount;
+var choice;
+var userChoice;
 
 
 function startTest() {
@@ -24,72 +26,93 @@ function startTest() {
   startPrompt.style.display = "none";
   questionArea.style.display = "block";
   correctCheck.style.display = "none";
-  randomQuestion = questions.sort ( () => Math.random() - 0.5);
-  console.log(randomQuestion);
-  currentQuestion = 0;
+  startButton.style.display = "none";
  nextQuestion ();
 }
 
 
+function checkAnswer () {
+
+ if (choiceClick !== questions[currentQuestionIndex].answer.innerText) {
+   timerCount -= 15;
+   timerElement.textContent = timerCount;
+   console.log ("incorrect");
+   currentQuestionIndex++;
+   nextQuestion();
+   //reflect if q was wrong
+ } else
+ console.log ("correct");
+  //  currentQuestionIndex++;
+   nextQuestion();
+ //write in if it's 
+ }
 
 function nextQuestion() {
-  showNextQuestion(randomQuestion[currentQuestion]); 
+  showNextQuestion(questions[currentQuestionIndex]); 
 }
 
-function showNextQuestion(question) {
-  questionEL.innerText = question.question;
-  option1.innerText = (question.options[0].text)
-  option2.innerText = (question.options[1].text)
-  option3.innerText = (question.options[2].text)
-  option4.innerText = (question.options[3].text)
+function choiceClick(clickedChoice) {
+   alert (clickedChoice.innerText); 
+   checkAnswer();
 }
-var questions = [
-  {
-    question: "Filler question 1",
-    options: [
-      { text: "Filler answer 1", isCorrect: true},
-      { text: "Filler answer 2", isCorrect: false},
-      { text: "Filler answer 3", isCorrect: false},
-      { text: "Filler answer 4", isCorrect: false},
-    ]
-    // question2: "Filler question 2",
-    // options2: [
-    //   { text: "Filler answer 1", correct: true},
-    //   { text: "Filler answer 2", correct: false},
-    //   { text: "Filler answer 3", correct: false},
-    //   { text: "Filler answer 4", correct: false},
-    // ],
-    // question3: "Filler question 3",
-    // options3: [
-    //   { text: "Filler answer 1", correct: true},
-    //   { text: "Filler answer 2", correct: false},
-    //   { text: "Filler answer 3", correct: false},
-    //   { text: "Filler answer 4", correct: false},
-    // ], 
-    // question4: "Filler question 4",
-    // options4: [
-    //   { text: "Filler answer 1", correct: true},
-    //   { text: "Filler answer 2", correct: false},
-    //   { text: "Filler answer 3", correct: false},
-    //   { text: "Filler answer 4", correct: false},
-    // ], 
-    // question5: "Filler question 5",
-    // options5: [
-    //   { text: "Filler answer 1", correct: true},
-    //   { text: "Filler answer 2", correct: false},
-    //   { text: "Filler answer 3", correct: false},
-    //   { text: "Filler answer 4", correct: false},
-    // ] 
-  }
-];
 
 
+function showNextQuestion(questions) {
+  questionEL.innerText = questions.title;
+  option1.innerText = (questions.choices[0])
+  option2.innerText = (questions.choices[1])
+  option3.innerText = (questions.choices[2])
+  option4.innerText = (questions.choices[3])
+}
+
+
+// function choiceClick(clickedChoice) {
+//   //alert (clickedChoice.innerText); 
+
+
+//    if handleChoice(clickedChoice)
+//   {
+//     // show correct
+
+//     // advance to next
+//   }
+//   else
+//   {
+//     //booo!
+
+//     // decrease timer
+
+//   }
+
+
+// }
+
+// function bool handleChoice() {
+//   // see if correct
+
+//   return true;
+
+// }
+
+//if timerCount < 0 then end game 
 
 function startTimer() {
   timer = setInterval(function () {
     timerCount--;
     timerElement.textContent = "Time: " + timerCount;
+    if (timerCount < 0){
+      clearInterval(timer);
+      alert ("You lost")
+      timerElement.textContent = "Time: 0";
+    }
   }, 1000);
 }
 
 startButton.addEventListener("click", startTest);
+
+//choice.addEventListener("click") = userChoice; 
+choice.addEventListener("click", choiceClick); 
+
+//complete question.js, make sure it's going through
+//check if the if statement and time decrease is working
+//check if the correct answer is working too
